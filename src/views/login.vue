2c6347c6cf11d79cce8141c73c52026368d9ffcc
Zhantrywei <zhantrywei@gmail.com>
2018-03-10 13:00:50 +0800

    增加登录页面

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,7 +1,33 @@
 <template>
-  <div class="login">
-      登录
-  </div>
+  <el-container class="login">
+    <el-header class="login-header">
+      <el-row>
+        <el-col :xs="24">
+          <h1>Maintance</h1>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :xs="24">
+          <h2 class="text-right">很高兴为你服务</h2>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main class="form">
+      <el-row>
+        <el-col :xs="24"><input type="text" placeholder="学号/手机号/邮箱账号" autofocus></el-col>
+      </el-row>
+      <el-row>
+        <el-col :xs="24"><input type="password" placeholder="密码" autofocus></el-col>
+      </el-row>
+      <el-row>
+        <el-col :xs="24"><button>登 录</button></el-col>
+      </el-row>
+      <el-row :gutter="10">
+        <el-col :xs="12"><router-link to="">忘记密码?</router-link></el-col>
+        <el-col :xs="12" class="text-right"><router-link to="">新用户注册</router-link></el-col>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
@@ -11,4 +37,35 @@ export default {
 </script>
 
 <style scoped>
+.login {
+    width: 100%;
+    height: 100%;
+    background-color: #e6e7e9;
+    background-image: url(/static/img/bgimg.jpg);
+    background-position: top center;
+    background-repeat: no-repeat;
+    background-size: contain;
+}
+.login-header {
+  margin-top: 300px;
+}
+.form input{
+  width: 100%;
+  height: 30px;
+  box-sizing: border-box;
+  border-bottom: lightgray 1px solid;
+  padding: 0px 5px;
+  border-radius: 4px;
+  margin-bottom: 10px;
+}
+.form button {
+  width: 100%;
+  height: 30px;
+  border-radius: 4px;
+  margin-bottom: 10px;
+  outline: none;
+}
+.text-right {
+  text-align: right;
+}
 </style>
